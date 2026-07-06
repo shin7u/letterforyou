29af3e6ae13646f36c7bdebcfee1a5492ad87f02
Shin7u <agam.shin7@gmail.com>
2026-07-07 03:10:09 +0700

    feat(app): connect loading intro and envelope

--- js/app.js
+++ js/app.js
@@ -2,18 +2,24 @@ import CONFIG from "./config.js";
 
 import SceneManager from "./engine/SceneManager.js";
 
+import LoadingScene from "./scenes/LoadingScene.js";
+import IntroScene from "./scenes/IntroScene.js";
 import EnvelopeScene from "./scenes/EnvelopeScene.js";
 
 console.clear();
 
 console.log(CONFIG.APP_NAME);
 
-const app =
-document.getElementById("app");
+const app = document.getElementById("app");
 
-const scene =
-new SceneManager(app);
+const scene = new SceneManager(app);
 
-scene.load(
-EnvelopeScene
-);
+(async () => {
+
+    await scene.load(LoadingScene);
+
+    await scene.load(IntroScene);
+
+    await scene.load(EnvelopeScene);
+
+})();
